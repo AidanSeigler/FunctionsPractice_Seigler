eda4b195c1bdfe363930d1fbb47493c52bb55543
Aidan Seigler <92833802+AidanSeigler@users.noreply.github.com>
2021-10-22 20:20:10 +0000

    Added menu along with velocity and acceleration calculator functions

--- main.cpp
+++ main.cpp
@@ -1,108 +1,93 @@
-/*
-	Input Validation with Ref Functions 
-	A cleaner way to do input validation
-	Validating input, an essential process.
-	Source: http://stackoverflow.com/questions/514420/how-to-validate-numeric-input-c
-	Keeping Dr. McMillan from breaking my code, defend against "dark side of the force",
-	and/or legitimate user error. 
-	//Try and Catch blocks are used in error checking
-	Teacher: Dr. Tyson McMillan, Ph.D.  
-  STUDENT (McMillanite):
-*/
-#include <cstdlib>  // Provides EXIT_SUCCESS
-#include <iostream>  // Provides cout, cerr, endl
-#include "Input_Validation_Extended.h" // Provides getValidatedInput<T>(), a header File with a Template Class
-                              // Extended 2-11-2016 by Dr. McMillan to add validation methods by datatype
-#include<string> //for string manipulation
-#include<sstream> //for string manipulation and comparison
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <stdlib.h>
+#include "Input_Validation_Extended.h"
 using namespace std;
+
+void showMenu();
+void velocityCalc();
+void accelerationCalc();
 
 int main()
 {
-   //***********Validating int data type******************/
-	int input = 0;
-	input = validateInt(input); //validate my integer by function call, save that value into input
-    cout << "You entered valid int: " << input << endl;
+  string input = "";
+
+  do
+  {
     
-	/****************Extend code to Validate double (follow int pattern)**********/
-	double inputDouble = 0.0;
-  	inputDouble = validateDouble(inputDouble); //validate my double by function call, save that value into inputDouble
-    cout << "You entered valid double: " << inputDouble << endl;
+    showMenu();
+    cout << "Choose an option: ";
+    input = validateString(input);
 
-	/****************Extend code to Validate char (follow int pattern)**********/
-	char inputChar = '\0'; //{0}
-	inputChar = validateChar(inputChar); //validate my char by function call, save that value into inputChar
-    cout << "You entered valid char: " << inputChar << endl;
+    if (input == "1")
+    {
+      velocityCalc();
+    }
+    else if (input == "2")
+    {
+      accelerationCalc();
+    }
+    else if (input == "X" || input == "x")
+    {
+      system("clear");
+    }
+    else if (input == "E" || input == "e")
+    {
+      cout << "Goodbye!\n";
+    }
+    else
+    {
+      cout << "Error processing input\n";
+    }
+    
+  } while (input != "E" && input != "e");
 
-	/****************Extend code to Validate string (follow int pattern, if possible)**********/
-	string inputString = (""); 
-	inputString = validateString(inputString); //validate my string by function call, save that value into inputString
-    cout << "You entered string: " << inputString << endl;
+  return 0;
+}
 
-	/****************Extend code to Validate if truly an int and greater than 77 **********/
-	int input2 = 0;
-	
-	do
-	{
-		cout << "Enter an integer greater than 77." << endl;  
-		input2 = validateInt(input2); //validate my integer by function call, save that value into input
-		
-		if(input2 < 77)
-		{
-			cout << "\nInvalid input: (" << input2 <<") number must be greater than 77." << endl;
-		}
-	}
-	while(input2 < 77); //loop until input2 is greather than 77
 
-    cout << "You entered valid int: " << input2 << endl;
+void showMenu()
+{
+  cout << "\n*PHYSICS CALCULATOR*\n\n";
+  cout << "1) Velocity\n";
+  cout << "2) Acceleration\n";
+  cout << "3) Motion\n";
+  cout << "4) Weight\n";
+  cout << "5) Momentum\n";
+  cout << "E) Exit\n";
+  cout << "X) Clear Screen\n";
+}
 
-	/****************Extend code to Validate if truly an double and greater than 0.00 **********/
-	double inputDouble2 = 0.0;
-	
-	do
-	{
-		cout << "Enter an number greater than 0."<< endl; 
-		inputDouble2 = validateDouble(inputDouble2); //validate my integer by function call, save that value into input
-		
-		if(inputDouble2 < 0)
-		{
-			cout << "\nInvalid input: (" << inputDouble2 <<") number must be greater than 0." << endl;
-		}
-	}
-	while(inputDouble2 < 0); //loop until inputDouble2 is greather than 0
+void velocityCalc()
+{
+  double ds, dt;
+  float velocity;
+  cout << "Enter a value for delta S (speed): ";
+  ds = validateDouble(ds);
 
-    cout << "You entered valid double: " << inputDouble2<< endl;
-    //A program to output grade values
-   //based upon numberical input of the user yourName: Teacher: Dr_T 8-28-2019
-   double grade = 0.0; 
-   do
-   { // begin do 
-      cout << "\nPlease enter a numerical grade (-1 to exit): ";
-      grade = validateDouble(grade); //accept grade and validate the data type
-      //if condition test
-      if(grade >= 90.0)
-      {
-        cout << "\nThat's an A!" << endl; 
-      }
-      else if(grade >= 80.0 && grade <= 89.9)
-      {
-        cout << "\nThat's a B!" << endl; 
-      }
-      else if(grade >= 70.0 && grade <= 79.9)
-      {
-        cout << "\nThat's a C!" << endl; 
-      }
-      else if(grade >= 60.0 && grade <= 69.9)
-      {
-        cout << "\nYikes. That's a D!" << endl; 
-      }
-      else if(grade < 60.0)
-      {
-        cout << "\nSee you next semester: F" << endl; 
-      }
-      else {cout << "\nInvalid input." << endl;}
+  cout << "Enter a value for delta T (time): ";
+  dt = validateDouble(dt);
 
-   }while(grade != -1); //end do 
-   
-    return 0;
+  velocity = ds / dt;
+  cout << "The velocity is " << fixed << setprecision(4) << velocity << endl;
+}
+
+void accelerationCalc()
+{
+  double dv, dt;
+  float acceleration;
+  cout << "Enter a value for delta V (velocity): ";
+  dv = validateDouble(dv);
+
+  cout << "Enter a value for delta T (time): ";
+  dt = validateDouble(dt);
+
+  acceleration = dv / dt;
+  cout << "The acceleration is " << fixed << setprecision(4) << acceleration << endl;
+}
+
+void motionCalc()
+{
+
 }
